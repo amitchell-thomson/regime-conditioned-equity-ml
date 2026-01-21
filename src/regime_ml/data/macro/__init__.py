d23557fdd438168da4cc49352b587ee4c589cdd3
amitchell-thomson <alec.mitchellthomson@gmail.com>
2026-01-21 15:08:54 +0000

    bugfix(circular import): fixed bug where "selectors" is a previously defined python module so throws circular import error

--- src/regime_ml/data/macro/__init__.py
+++ src/regime_ml/data/macro/__init__.py
@@ -3,7 +3,7 @@
 from .loaders import load_raw_data
 from .cleaners import clean_data, roll_weekend_releases, trim_to_common_start
 from .alignment import add_staleness_indicators, align_to_calendar
-from .selectors import select_data
+from .selector import select_data
 from .validators import validate_data, print_validation_report
 from .pipeline import run_macro_pipeline
 
